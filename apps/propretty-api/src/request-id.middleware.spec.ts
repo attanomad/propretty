9a2d60ae30250046f81e0a2beb0a46656666739c
nawawishkid <nawawish.samerpark@gmail.com>
2024-09-19 00:55:49 +0700

    remove unused request id middleware

--- apps/propretty-api/src/request-id.middleware.spec.ts
+++
@@ -1,7 +0,0 @@
-import { RequestIdMiddleware } from './request-id.middleware';
-
-describe('RequestIdMiddleware', () => {
-  it('should be defined', () => {
-    expect(new RequestIdMiddleware()).toBeDefined();
-  });
-});
