4899331c8947352f0b9a60c76760fea9cb1137c4
nawawishkid <nawawish.samerpark@gmail.com>
2024-09-19 00:59:39 +0700

    fix(api) :bug: remove manual global interceptor registration

--- apps/propretty-api/src/main.ts
+++ apps/propretty-api/src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { UnifiedResponseInterceptor } from './unified-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.useGlobalInterceptors(app.get(UnifiedResponseInterceptor));
 
   await app.listen(3002);
 }
